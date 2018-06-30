31edc1677cb4845e7ffd235bcf4d8feb99788602
Mayuran <mayuran29@gmail.com>
2018-06-30 16:07:40 -0400

    Code Added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,6 +14,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            // driver.FindElement(By.XPath("//*[@id='nav-link-shopall']/span[2]/span")).Click();
+            // driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("Hello");
+
+            //IWebElement month_dropdown = driver.FindElement(By.Id("month"));
+
+            var day = driver.FindElement(By.Id("day"));
+            var select_day = new SelectElement(day);
+            select_day.SelectByValue("2");
+
+            var month = driver.FindElement(By.Id("month"));
+            var select_month = new SelectElement(month);
+            select_month.SelectByText("Apr");
+
+
+
+
+            Thread.Sleep(10000);
+
+            driver.Quit();
         }
     }
 }
